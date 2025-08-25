b466114fb0efba855e59827b5593bb9e3ee9fa83
natwerth <165090625+natwerth@users.noreply.github.com>
2025-08-25 15:16:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configure your Worker endpoint here or via the UI button.
 // Example: https://firemebot-worker.nat-1fa.workers.dev/analyze
 window.FireMeBot = {
-  WORKER_URL: "https://firemebot-worker.nat-1fa.workers.dev/analyze" // leave empty and set via the 'Set endpoint' button if deploying on GitHub Pages
+  WORKER_URL: "https://firemebot-worker.nat-1fa.workers.dev/api/roast" // leave empty and set via the 'Set endpoint' button if deploying on GitHub Pages
 };
